90b786fa853819ab4d5686e2fe194c5dfecbbdeb
gireeswar18 <gireeswarcp18@gmail.com>
2025-02-04 09:03:48 +0530

    04/02/2025

---
+++ LowestCommonAncestorOfABinarySearchTree.java
@@ -0,0 +1,14 @@
+package neetcode150;
+
+public class LowestCommonAncestorOfABinarySearchTree {
+
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+		if (root.val > p.val && root.val > q.val) {
+			return lowestCommonAncestor(root.left, p, q);
+		}
+		if (root.val < p.val && root.val < q.val) {
+			return lowestCommonAncestor(root.right, p, q);
+		}
+		return root;
+	}
+}
